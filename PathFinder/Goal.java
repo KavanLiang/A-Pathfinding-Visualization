cf8d6c4a0d8003edb68ccc96753d33ddf8e6b195
Kavan Liang <kavanliang@gmail.com>
2018-01-13 13:22:10 -0500

    Update Goal.java

--- PathFinder/Goal.java
+++ PathFinder/Goal.java
@@ -2,20 +2,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.awt.Color;
 /**
  * @Author Kavan
+ * 
+ * A Goal for the Pathfinder to Navigate to.
+ * 
+ * @param PIXEL_SIZE: The height and width of this Goal
+ * @param OFFSET: Offset for movement
  */
-public class Goal extends Actor
-{
+public class Goal extends Actor {
+    
     private static final int PIXEL_SIZE = 30;
     private static final int OFFSET = 1;
-    public Goal()
-    {
+
+    /**
+     * Create a new Goal
+     */
+    public Goal(){
         GreenfootImage wall = new GreenfootImage(PIXEL_SIZE, PIXEL_SIZE);
         wall.setColor(Color.green);
         wall.fill();
-        this.setImage(wall);
+        this.setImage(wall); 
     }
-    public void moveGoal()
-    {
+
+    /**
+     * Move the goal around the world
+     */
+    public void moveGoal(){
         if(Greenfoot.isKeyDown("w"))
             setLocation(getX(), getY() - OFFSET);
         if(Greenfoot.isKeyDown("a"))
@@ -25,8 +36,8 @@ public class Goal extends Actor
         if(Greenfoot.isKeyDown("d"))
             setLocation(getX() + OFFSET, getY());
     }
-    public void act() 
-    {
+    
+    public void act(){
         moveGoal();// Add your action code here.
-    }    
+    }
 }
